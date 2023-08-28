7c091e9210d7b92c57ae39f8324225fa96d4c1af
tobiwashi <tobiigberase@gmail.com>
2023-08-28 00:30:39 -0400

    About me Finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
-
+import '../App.css'
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div className='welcome'>
+                <h1>
+                    <span>Tobi Igberase</span>
+                </h1>
+                <h2 className=''>
+                    <span className='text1'>Software Engineer</span>
+                    <span className='text2'>Video Editor</span>
+                </h2>
+            </div>
+            <div className='about-me'>
+                <img className='avatar' src='/images/Avatar.png'></img>
+            </div>
+            <h3 className='about-text'>About Me</h3>
+
+            <h4 className='about-subtext'>
+                I'm Tobi Igberase, a software engineer based in the Richmond Metropolitan Area. I have a passion
+                for problem-solving, innovation, and automation. My skills include but are not limited to Fullstack
+                Development, SQL and NoSQL Database Querying/Management, Data Structures and Algorithms, and Cloud Computing (AWS).
+            </h4>
+        </>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,34 @@
+import { Link } from "react-router-dom"
 
-
-function Navbar() {
+function Navbar(props) {
 
     return (
         <>
             <div className="navbar">
                 <div className="nav-items">
-                    <img className="nav-icon" src="\images\home-icon.png" alt='Home Icon'></img>
-                    <span>Home</span>
+                    <img className="icon" src="images/home-icon.png" alt="Home Icon"></img>
+                    <span><Link to='/'></Link>Home</span>
+
                 </div>
                 <div className="nav-items">
-                    <img className="nav-icon" src="\images\project-icon.png" alt='Home Icon'></img>
-                    <span>Projects</span>
+                    <img className="icon" src="images/project-icon.png" alt="Project Icon"></img>
+                    <span><Link to='/Projects'></Link>Projects</span>
                 </div>
                 <div className="nav-items">
-                    <img className="nav-icon" src="\images\resume-icon.png" alt='Home Icon'></img>
-                    <span>Resume</span>
+
+                    <img className="icon" src="images/resume-icon.png" alt="Resume Icon"></img>
+                    <span><Link to='/'></Link>Resume</span>
+                </div>
+            </div>
+
+            {props.children}
+
+            <div className="footer">
+                <span>®Tobiwashi Visual Studios</span>
+                <div className="footer-items">
+                    <img className="footer-icon" src="images/LinkedIn.svg" alt="LinkedIn Icon"></img>
+                    <img className="footer-icon" src="images/Github.svg" alt="Github Icon"></img>
+                    <img className="footer-icon" src="images/Youtube.svg" alt="Youtube Icon"></img>
                 </div>
             </div>
         </>
